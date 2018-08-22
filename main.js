244934774c09f3438de02f0ab9224ab8332de4de
Murage <murageerick@gmail.com>
2018-08-22 20:01:49 +0300

    Adjustments to dropdown menu

--- main.js
+++ main.js
@@ -15,14 +15,15 @@ $(document).ready(function() {
 });
 //Drop-down menu.
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById('myDropdown').classList.toggle('show');
 }
-window.onclick = function (event) {
+
+window.onclick = function(event) {
     if (!event.target.matches('.drop-btn')) {
-        let dropdowns = document.getElementById("myDropdown");
-        var i;
-        for (i = 0; i < dropdowns.length; i++){
-            var openDropdown = dropdowns[i];
+        const dropdowns = document.getElementsByClassName(dropdown-content);
+        
+        for (let i = 0; i < dropdowns.length; i++){
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')){
                 openDropdown.classList.remove('show');
             }
